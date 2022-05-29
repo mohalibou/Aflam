4d15b5913c8b003100a132a57423d732b6243ccf
mohalibou <mohalibou@icloud.com>
2022-05-29 13:40:21 -0700

    Initial Commit

---
+++ Aflam/AflamApp.swift
@@ -0,0 +1,17 @@
+//
+//  AflamApp.swift
+//  Aflam
+//
+//  Created by Mohamed Ali Boutaleb on 5/29/22.
+//
+
+import SwiftUI
+
+@main
+struct AflamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
